e91f51bcc5baa78560e968c6ec81c67b0c251772
ash7676 <akshaystech2233@gmail.com>
2024-03-02 11:47:56 +0530

    second challenge finshed

--- hack/2. cleaning-robot/javascript/challenge.js
+++ hack/2. cleaning-robot/javascript/challenge.js
@@ -1,9 +1,18 @@
 // Robot controller implementation
 
-import { robotControl, Direction } from './robot-api.js';
+import { robotControl, Direction } from "./robot-api.js";
 
 const rc = robotControl("../resources/room-layout-1.txt");
 const api = rc.robotApi;
+// console.log("move--------",api.move())
+// console.log("current DIR-----",api.getDirection());
+// console.log("change dir---",api.turnLeft());
+// console.log("new current DIR-----",api.getDirection());
+// console.log("this turn left---",api.turnLeft())
+// console.log("show left---",api.getDirection())
+// console.log("get pos-----",api.getPosition());
+// console.log("get position ahead-----",api.getPositionAhead());
+
 cleanRooms(api);
 rc.evaluateResult();
 
@@ -18,5 +27,71 @@ rc.evaluateResult();
 // api.getPositionAhead(); // returns the position in front of the robot, doesn't check if a barrier
 
 function cleanRooms(api) {
-    // TODO add code here
+  // TODO add code here
+
+  // matrix size is  7* 5
+
+  console.log("robot initial position:---", api.getPosition());
+  console.log("robot initial directiion------", api.getDirection());
+  api.turnLeft();
+  api.turnLeft();
+  console.log("robot new directiion------", api.getDirection()); //left
+  console.log("ne dir after mvoing---", api.getPosition());
+
+  //clean first row
+  //DIR= LEFT
+  for (let r1 = 0; r1 <= 3; r1++) {
+    api.move();
+  }
+
+  console.log("position after first row-----", api.getPosition());
+  //DIR DOWN -> DIR LEFT
+  //CLEAN SECOND ROW
+  api.turnLeft(); //DOWN
+  api.move();
+  api.turnLeft();
+  console.log("dir2=---", api.getDirection());
+  for (let r2 = 0; r2 <= 4; r2++) {
+    api.move();
+  }
+
+  console.log(" position after cleaning second row----", api.getPosition());
+  //7,1
+  api.turnRight()
+  api.move();
+  api.turnRight()
+
+  for (let r2 = 0; r2 <= 4; r2++) {
+    api.move();
+  }
+  console.log("dir after cleaning thrid row---", api.getDirection());
+
+  api.turnLeft();
+  api.move();
+  api.turnLeft()
+  console.log("dir now11-----",api.getDirection())
+  console.log("pos------",api.getPosition())
+  for(let i=0; i<=3; i++){
+    api.move()
+  }
+  console.log("POST-----",api.getPosition())
+  api.turnLeft();
+  api.turnLeft();
+  for(let i=0; i<=7; i++){
+    api.move()
+  }
+  console.log("POSSS----",api.getPosition())
+  console.log("dir---",api.getDirection())
+
+  api.turnLeft();
+  api.move();
+  console.log("POSSS----",api.getPosition())
+  console.log("dir---",api.getDirection())
+  api.turnLeft();
+
+  console.log("DIRRR----",api.getDirection())
+  for(let i=0; i<=6; i++){
+    api.move()
+  }
 }
+
